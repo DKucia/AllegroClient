7a9298fc4de0fb37ea44bee9131f059a29eec67e
dkucia <dkucia@unit-soft.pl>
2020-05-21 14:24:02 +0200

    Add fetch token ,enviroment settings

---
+++ AllegroClient/TokenResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AllegroClient
+{
+    public class TokenResponse
+    {
+        [Newtonsoft.Json.JsonProperty("access_token")]
+        public string AccessToken { get; set; }
+        [Newtonsoft.Json.JsonProperty("refresh_token")]
+        public string RefreshToken { get; set; }
+        [Newtonsoft.Json.JsonProperty("expires_in")]
+        public int ExpireTime { get; set; }
+    }
+}
